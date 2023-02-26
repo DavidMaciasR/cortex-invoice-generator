7def3273677249b5ab2c5af9c9afedc6f4eb0a0a
Me <david414tecno@gmail.com>
2023-02-26 20:58:40 +0000

    chore: updating name app

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular-pdfmake-invoice app is running!');
+    expect(page.getTitleText()).toEqual('cortex-invoice-generator app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-pdfmake-invoice'`, () => {
+  it(`should have as title 'cortex-invoice-generator'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-pdfmake-invoice');
+    expect(app.title).toEqual('cortex-invoice-generator');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-pdfmake-invoice app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('cortex-invoice-generator app is running!');
   });
 });
